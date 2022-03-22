c58be39761fd3c1c307281eb4e3f206bf6c5fe53
Gopibabu Srungavarapu <s.gopibabu@gmail.com>
2022-03-22 08:38:18 -0500

    added vuejs support

---
+++ resources/js/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="main">
+    <h1 class="text-xl text-center">{{ message }}</h1>
+</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: "Dockerized Laravel Application with Vue.js"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.main {
+    display: block;
+}
+</style>
